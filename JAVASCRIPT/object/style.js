c1e0fac8a3cbf8bb40e3fd4d677c6606b63098b2
Diksha Narayan <214080489+Diksha-3011@users.noreply.github.com>
2026-08-11 21:10:22 +0530

    add switch

---
+++ JAVASCRIPT/object/style.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+// object creation
+let rectangle={
+    length:4,
+    width:5,
+
+    //method
+    draw:function(){
+        console.log("I am a rectangle");
+    }
+};
