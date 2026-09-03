1e6bddcb4335ec8b8f09ebdb4efe5416f6fd0177
Diksha Narayan <214080489+Diksha-3011@users.noreply.github.com>
2026-09-03 14:22:59 +0530

    egma6

--- JAVASCRIPT/promise.js
+++ JAVASCRIPT/promise.js
@@ -1,8 +1,24 @@
 function f1(){
 
+    return new Promise(function(resolve, reject){   
+  setTimeout(function(){
+    console.log("f1");
+    resolve();  
+  }, 1000);
+});
 }
-function f2(){
 
+function f2(){
+  return new Promise(function(resolve, reject){
+    setTimeout(function(){
+      console.log("f2");
+      resolve();
+    }, 2000);
+  });
 }
-f1();
+f1().then(f2)
+    .catch((err)=>{
+        console.log(err);
+    });
+
 f2();
